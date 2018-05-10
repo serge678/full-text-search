8bf078d9d28dfeafb89219e1469acdb9b5b20b88
Sergey Pronkin <pronkine@gmail.com>
2018-05-10 17:54:17 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='full_text_search',
+    version='1.0.0',
+    description='Full Text Search Index Based on N-grams',
+    url='https://github.com/spronkine/full-text-search',
+    author='Sergey Pronkin',
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Search Engines',
+        'License :: Public Domain',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='text retrieval search index full fuzzy',
+    packages=['full_text_search'],
+    install_requires=[],
+)
